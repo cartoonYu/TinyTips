93e4a1b7c05fd6803155df13351d1f095b10507f
Leo <752992071@qq.com>
2018-08-12 17:26:51 +0800

    parts of homepages

---
+++ app/src/main/java/com/cartoon/tinytips/util/Adapters/MajorAdapter.java
@@ -0,0 +1,63 @@
+package com.cartoon.tinytips.util.Adapters;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.cartoon.tinytips.R;
+
+import java.util.Iterator;
+import java.util.List;
+
+public class MajorAdapter extends RecyclerView.Adapter<MajorAdapter.ViewHolder> {
+    private Context mContext;
+
+    private List<Major> mMJList;
+
+    static class ViewHolder extends RecyclerView.ViewHolder {
+        CardView cardView;
+        ImageView majorImage;
+        TextView majorName;
+
+        public ViewHolder(View view) {
+            super(view);
+            cardView = (CardView) view;
+            majorImage = (ImageView) view.findViewById(R.id.class_image);
+            majorName = (TextView) view.findViewById(R.id.class_name);
+        }
+    }
+
+    public MajorAdapter(List<Major> MJList) {
+        this.mMJList = MJList;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        if (mContext == null) {
+            mContext = parent.getContext();
+        }
+        View view = LayoutInflater.from(mContext).inflate(R.layout.major_item, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        Major major = mMJList.get(position);
+        holder.majorName.setText(major.getName());
+        Glide.with(mContext).load(major.getImageId()).into(holder.majorImage);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mMJList.size();
+    }
+
+}
