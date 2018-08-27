280c56fea64bbaa5a1e36e2b5d5f6d906496a812
Leo <752992071@qq.com>
2018-08-27 16:17:54 +0800

    部分布局稍作修改，Item添加点击事件

--- app/src/main/java/com/cartoon/tinytips/util/Adapters/Homepage/FavoriteAdapter.java
+++ app/src/main/java/com/cartoon/tinytips/util/Adapters/Homepage/FavoriteAdapter.java
@@ -6,24 +6,33 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.cartoon.tinytips.HomePage.Favorite.Favorite;
 import com.cartoon.tinytips.HomePage.Favorite.FavoriteItem;
+import com.cartoon.tinytips.Note.Comment.Comment;
+import com.cartoon.tinytips.Note.ShareNote.ShareNote;
 import com.cartoon.tinytips.R;
+import com.cartoon.tinytips.util.Adapters.Tips.IOnItemClickListener;
+import com.cartoon.tinytips.util.IntentActivity;
 
 import java.util.List;
 
-public class FavoriteAdapter extends RecyclerView.Adapter<FavoriteAdapter.ViewHolder>{
+import butterknife.BindView;
+
+import static com.cartoon.tinytips.util.TinyTipsApplication.getContext;
+
+public class FavoriteAdapter extends RecyclerView.Adapter<FavoriteAdapter.ViewHolder> implements View.OnClickListener{
     private Context mContext;
 
     private List<FavoriteItem> mFavoriteItems;
+    private IOnItemClickListener mClickListener;
 
-
-
-    static class ViewHolder extends RecyclerView.ViewHolder {
+    static class ViewHolder extends RecyclerView.ViewHolder  {
         ImageView userImages;
         TextView userNames;
         TextView titles;
@@ -33,6 +42,12 @@ public class FavoriteAdapter extends RecyclerView.Adapter<FavoriteAdapter.ViewHo
         TextView NumOfCollectoins;
         TextView NumOfShares;
         TextView time;
+        Button favoritenums_item_favorite;
+        Button commuitenums_item_favorite;
+        Button Cnums_item_favorite;
+        Button sharenums_item_favorite;
+
+        private IOnItemClickListener mListener;
 
         public ViewHolder(View view) {
             super(view);
@@ -45,6 +60,42 @@ public class FavoriteAdapter extends RecyclerView.Adapter<FavoriteAdapter.ViewHo
             NumOfCollectoins = (TextView)view.findViewById(R.id.Fitem_numofcollection);
             NumOfShares = (TextView)view.findViewById(R.id.Fitem_numofshare);
             time = (TextView)view.findViewById(R.id.Fitem_time);
+            favoritenums_item_favorite = (Button)view.findViewById(R.id.favoritenums_item_favorite);
+            commuitenums_item_favorite= (Button)view.findViewById(R.id.commuitenums_item_favorite);
+            Cnums_item_favorite= (Button)view.findViewById(R.id.Cnums_item_favorite);
+            sharenums_item_favorite= (Button)view.findViewById(R.id.sharenums_item_favorite);
+        }
+
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.favoritenums_item_favorite:{
+                Button favoritenums_item_favorite;
+                favoritenums_item_favorite = (Button)view.findViewById(R.id.favoritenums_item_favorite);
+                favoritenums_item_favorite.setBackgroundResource(R.drawable.favorite_press);
+                break;
+            }
+
+            case R.id.commuitenums_item_favorite:{
+                IntentActivity.intentWithoutData(getContext(),Comment.class);
+                break;
+            }
+
+            case R.id.Cnums_item_favorite:{
+                Button Cnums_item_favorite;
+                Cnums_item_favorite= (Button)view.findViewById(R.id.Cnums_item_favorite);
+                Cnums_item_favorite.setBackgroundResource(R.drawable.mycollection_press);
+                break;
+            }
+
+            case R.id.sharenums_item_favorite:{
+                IntentActivity.intentWithoutData(getContext(),ShareNote.class);
+                break;
+            }
+
         }
     }
 
@@ -73,12 +124,18 @@ public class FavoriteAdapter extends RecyclerView.Adapter<FavoriteAdapter.ViewHo
         holder.NumOfFavoirtes.setText(""+ FavoriteItem.getNumOfFavoirte());
         holder.time.setText(""+FavoriteItem.getTime());
         holder.NumOfShares.setText(""+FavoriteItem.getNumOfShare());
+        holder.favoritenums_item_favorite.setOnClickListener(this);
+        holder.commuitenums_item_favorite.setOnClickListener(this);
+        holder.Cnums_item_favorite.setOnClickListener(this);
+        holder.sharenums_item_favorite.setOnClickListener(this);
         Glide.with(mContext).load(FavoriteItem.getuserImage()).into(holder.userImages);
-
     }
 
     @Override
     public int getItemCount() {
         return mFavoriteItems.size();
     }
+
+
+
 }
